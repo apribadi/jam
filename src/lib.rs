c1246f1b6de33edb8c79f53c57a4d7bfc4ae992a
Aaron Pribadi <aaron.pribadi@gmail.com>
2023-06-19 18:29:49 -0700

    z

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,1 @@
-#![allow(non_camel_case_types)]
-#![deny(unsafe_op_in_unsafe_fn)]
-#![warn(elided_lifetimes_in_paths)]
-#![warn(non_ascii_idents)]
-#![warn(trivial_numeric_casts)]
-#![warn(unreachable_pub)]
-#![warn(unused_lifetimes)]
-#![warn(unused_qualifications)]
-#![warn(unused_results)]
-
 pub mod runtime;
